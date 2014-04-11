b7e24d72cafe057288be82677aeb3c1b466bbcd3
kolenda <admin@kolenda.me>
2014-04-11 15:31:33 +0200

    typo

--- main.cpp
+++ main.cpp
@@ -324,7 +324,7 @@ void drawTris( float x, float y, float z, MarchingCubes::TriangleF* tris, int tr
 //		MarchingCubes::Vector3F  delta2 = tri.v[2].pos - tri.v[0].pos;
 //		MarchingCubes::Vector3F  normal;
 //		MarchingCubes::getCrossProduct( delta1.f, delta2.f, normal.f );
-		MarchingCubes::Vector3F  normal = MarchingCubes::getTriangleNormal( tri.v[0], tri.v[1], tri.v[2] );
+		MarchingCubes::Vector3F  normal = MarchingCubes::getTriangleNormal( tri.v[0].pos, tri.v[1].pos, tri.v[2].pos );
 
 		glNormal3f( normal.f[0], normal.f[1], normal.f[2] );
 
